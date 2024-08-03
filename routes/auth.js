dda8fe6dd55d97a62164bf9b59100096e000c09f
Alex Singh <115970252+singhalex@users.noreply.github.com>
2024-08-03 14:39:12 -0700

    Add a logout route and sign up route

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,13 @@ const authController = require("../controllers/authcontroller");
 // Authenticate user on POST
 router.post("/log-in", authController.login_post);
 
+// Show log in form on GET
+router.get("/sign-up", authController.signup_get);
+
 // Create user on POST
 router.post("/sign-up", authController.signup_post);
+
+// Log user out on GET
+router.get("/log-out", authController.logout_get);
 
 module.exports = router;
