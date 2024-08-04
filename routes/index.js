33f46322df5f87dc4c2772a660be0d203daa9a80
Alex Singh <115970252+singhalex@users.noreply.github.com>
2024-08-04 08:58:18 -0700

    Retreieve and pass user folders

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require("express");
 var router = express.Router();
 
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index");
+router.get("/", async function (req, res, next) {
+  let folders;
+  if (req.user) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: req.user.id,
+      },
+      include: {
+        folders: true,
+      },
+    });
+
+    folders = user.folders;
+  }
+  res.render("index", { folders });
 });
 
 module.exports = router;
