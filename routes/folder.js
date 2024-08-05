72fac58b4c1ef831520d94a2da32404bf7c04c87
Alex Singh <115970252+singhalex@users.noreply.github.com>
2024-08-05 14:55:53 -0700

    Add full CRUD operations to router

--- routes/folder.js
+++ routes/folder.js
@@ -5,12 +5,25 @@ const prisma = new PrismaClient();
 
 const folderController = require("../controllers/folderController");
 
+// Create a new folder on POST
 router.post("/", folderController.folder_post);
 
-router.delete("/", (req, res, next) => {
-  res.send("DELETED");
-});
-
+// Render folder details on GET
 router.get("/:id", folderController.single_folder_get);
+
+// Render folder delete form on GET
+router.get("/:id/delete", folderController.delete_folder_get);
+
+// Delete folder on POST
+router.post("/:id/delete", folderController.delete_folder_post);
+
+// Render rename folder form GET
+router.get("/:id/rename", folderController.rename_folder_get);
+
+// Rename folder on POST
+router.post("/:id/rename", folderController.rename_folder_post);
+
+// Upload file to folder on POST
+router.post("/:id", folderController.upload_to_folder_post);
 
 module.exports = router;
