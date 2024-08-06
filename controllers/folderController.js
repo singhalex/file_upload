f27e9a2cfdd66d9c2ca0e3c5bdd53517ff394037
Alex Singh <115970252+singhalex@users.noreply.github.com>
2024-08-05 21:16:37 -0700

    Rename folder controller

--- controllers/folderController.js
+++ controllers/folderController.js
@@ -129,9 +129,62 @@ exports.rename_folder_get = [
 ];
 
 // Rename folder on POST
-exports.rename_folder_post = (req, res, next) => {
-  res.send("Folder renamed");
-};
+exports.rename_folder_post = [
+  authorizeFolder,
+  body("newName")
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage("Folder name cannot be blank"),
+  body("newName").custom(async (value, { req }) => {
+    // Get all user folders
+    const userFolders = await prisma.user.findUnique({
+      where: {
+        id: req.user.id,
+      },
+      select: {
+        folders: true,
+      },
+    });
+
+    // Extract array of folders
+    const folders = userFolders.folders;
+    // See if input value is found in array
+    const match = folders.find((folder) => folder.name === value);
+    if (match) {
+      throw new Error("Folder name already exists");
+    }
+  }),
+  asyncHandeler(async (req, res, next) => {
+    const { errors } = validationResult(req);
+    if (errors.length > 0) {
+      // Get folder from db
+      const folder = await prisma.folder.findUnique({
+        where: {
+          id: parseInt(req.params.id),
+        },
+      });
+      return res.render("folder-view", {
+        errors,
+        mode: "rename",
+        folder,
+        folderNameValue: req.body.newName,
+      });
+    }
+
+    // Update folder name in db
+    const updatedFolder = await prisma.folder.update({
+      where: {
+        id: parseInt(req.params.id),
+      },
+      data: {
+        name: req.body.newName,
+      },
+    });
+    res.render("folder-view", { mode: "rename", folder: updatedFolder });
+  }),
+];
+
 // Upload file to folder on POST
 exports.upload_to_folder_post = [
   upload.single("file"),
