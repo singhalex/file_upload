6823d77289231ce6eda01a25cc54b1bb220ce575
Alex Singh <115970252+singhalex@users.noreply.github.com>
2024-08-22 15:53:16 -0700

    Rename uploaded files and render the page again

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,16 +1,30 @@
 const expressAsyncHandler = require("express-async-handler");
 
 const multer = require("multer");
-const upload = multer({ dest: "public/uploads/" });
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/uploads");
+  },
+  filename: (req, file, cb) => {
+    const fullFileName = file.originalname.split(".");
+    const truncFileName = fullFileName[0];
+    const extension = fullFileName[1];
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, truncFileName + "-" + uniqueSuffix + "." + extension);
+  },
+});
+const upload = multer({ storage });
 const mime = require("mime-types");
 
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+const retreiveFolder = require("../utils/retrieveFolder");
 
 // Upload file to folder on POST
 exports.upload_file_post = [
   upload.single("file"),
   expressAsyncHandler(async (req, res, next) => {
+    console.log(req.file);
     console.log(mime.extension(req.file.mimetype));
     const file = await prisma.file.create({
       data: {
@@ -27,7 +41,8 @@ exports.upload_file_post = [
     next();
   }),
 
+  retreiveFolder,
   (req, res, next) => {
-    res.send("UPLOAD FILE HERE");
+    res.render("folder-view", { mode: "get", folder: req.folder });
   },
 ];
