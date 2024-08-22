50bba609220296aee1c0e81c5a86d4688fd0e1bb
Alex Singh <115970252+singhalex@users.noreply.github.com>
2024-08-22 11:36:05 -0700

    Create retrieve folder utility function

---
+++ utils/retrieveFolder.js
@@ -0,0 +1,30 @@
+const asyncHandeler = require("express-async-handler");
+const formatBytes = require("../utils/formatBytes");
+const { DateTime } = require("luxon");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+// Utility to retreive a specific folder and attach it to the req object
+const retrieveFolder = asyncHandeler(async (req, res, next) => {
+  const folder = await prisma.folder.findUnique({
+    where: {
+      id: parseInt(req.params.id),
+    },
+    include: {
+      files: true,
+    },
+  });
+
+  // Format files for display
+  folder.files.forEach((file) => {
+    file.name = file.name.split(".")[0];
+    file.size = formatBytes(file.size);
+    file.date = DateTime.fromJSDate(file.date).toLocaleString(
+      DateTime.DATE_SHORT
+    );
+  });
+  req.folder = folder;
+  next();
+});
+
+module.exports = retrieveFolder;
