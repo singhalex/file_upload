465266ea91dd4e818e7b64da9329446c3497656a
Alex Singh <115970252+singhalex@users.noreply.github.com>
2024-08-23 13:50:22 -0700

    Add upload file method

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,46 +1,51 @@
 const expressAsyncHandler = require("express-async-handler");
+const { v4: uuidv4 } = require("uuid");
+const retreiveFolder = require("../utils/retrieveFolder");
 
+// Initialize multer and store file in memory
 const multer = require("multer");
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "public/uploads");
-  },
-  filename: (req, file, cb) => {
-    const fullFileName = file.originalname.split(".");
-    const truncFileName = fullFileName[0];
-    const extension = fullFileName[1];
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, truncFileName + "-" + uniqueSuffix + "." + extension);
-  },
-});
+const storage = multer.memoryStorage();
 const upload = multer({ storage });
-const mime = require("mime-types");
 
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-const retreiveFolder = require("../utils/retrieveFolder");
+
+const { createClient } = require("@supabase/supabase-js");
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 // Upload file to folder on POST
 exports.upload_file_post = [
   upload.single("file"),
   expressAsyncHandler(async (req, res, next) => {
-    console.log(req.file);
-    console.log(mime.extension(req.file.mimetype));
+    // Create unique file name
+    const fullFileName = req.file.originalname.split(".");
+    const truncFileName = fullFileName[0];
+    const fileExtenstion = fullFileName[1];
+    const uniqueFileName = `${truncFileName}-${uuidv4()}.${fileExtenstion}`;
+
+    // Upload file to supabase
+    const { data, error } = await supabase.storage
+      .from("files")
+      .upload(`${req.user.id}/${uniqueFileName}`, req.file.buffer);
+    if (error) {
+      // TODO - HANDLE ERROR HERE //
+    }
+
+    // Store file info in db
     const file = await prisma.file.create({
       data: {
         name: req.file.originalname,
         size: req.file.size,
-        url: req.file.path,
         date: new Date(),
-        type: mime.extension(req.file.mimetype),
+        type: fileExtenstion,
         folderId: parseInt(req.params.id),
         userId: req.user.id,
       },
     });
-    console.log(file);
     next();
   }),
-
   retreiveFolder,
   (req, res, next) => {
     res.render("folder-view", { mode: "get", folder: req.folder });
