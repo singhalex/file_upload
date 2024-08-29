18239a2c2e2b604c133e765a5eb76251f5785370
Alex Singh <115970252+singhalex@users.noreply.github.com>
2024-08-29 12:28:01 -0700

    Implemeent file download

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,6 +1,8 @@
 const expressAsyncHandler = require("express-async-handler");
 const { v4: uuidv4 } = require("uuid");
 const retreiveFolder = require("../utils/retrieveFolder");
+const authorizeFolder = require("../utils/authorizeFolder");
+const authorizeFile = require("../utils/authorizeFile");
 
 // Initialize multer and store file in memory
 const multer = require("multer");
@@ -15,14 +17,17 @@ const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
+const stream = require("stream");
+
 // Upload file to folder on POST
 exports.upload_file_post = [
+  authorizeFolder,
   upload.single("file"),
   expressAsyncHandler(async (req, res, next) => {
     // Create unique file name
     const fullFileName = req.file.originalname.split(".");
-    const truncFileName = fullFileName[0];
-    const fileExtenstion = fullFileName[1];
+    const fileExtenstion = fullFileName.pop();
+    const truncFileName = fullFileName.join(".");
     const uniqueFileName = `${truncFileName}-${uuidv4()}.${fileExtenstion}`;
 
     // Upload file to supabase
@@ -37,9 +42,10 @@ exports.upload_file_post = [
     const file = await prisma.file.create({
       data: {
         name: req.file.originalname,
+        uniqueName: uniqueFileName,
         size: req.file.size,
         date: new Date(),
-        type: fileExtenstion,
+        type: req.file.mimetype,
         folderId: parseInt(req.params.id),
         userId: req.user.id,
       },
@@ -50,4 +56,34 @@ exports.upload_file_post = [
   (req, res, next) => {
     res.render("folder-view", { mode: "get", folder: req.folder });
   },
+];
+
+exports.download_file_get = [
+  authorizeFile,
+  expressAsyncHandler(async (req, res, next) => {
+    // Retrieve raw data from supabase
+    const { data, error } = await supabase.storage
+      .from("files")
+      .download(`${req.user.id}/${req.uniqueName}`);
+
+    if (error) {
+      throw error;
+    }
+
+    // Convert raw data to readable file
+    const buffer = Buffer.from(await data.arrayBuffer());
+    const fileContents = Buffer.from(buffer, "base64");
+    const readStream = new stream.PassThrough();
+    readStream.end(fileContents);
+
+    // Set headers
+    const setHeader = {
+      "Content-Disposition": `attachment; filename=${req.originalFileName}`,
+      "Content-Type": req.type,
+    };
+    res.set(setHeader);
+
+    // Trigger download
+    readStream.pipe(res);
+  }),
 ];
