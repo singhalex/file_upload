e5ea0085fd36d7ce3010dbf1515cb1d470cee1cf
Alex Singh <115970252+singhalex@users.noreply.github.com>
2024-08-30 20:54:30 -0700

    Increase length of cookie expiration

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(
   session({
     cookie: {
-      maxAge: 60 * 60 * 1000, // ms,
+      maxAge: 7 * 24 * 60 * 60 * 1000, // ms,
     },
     secret: process.env.SESSION_SECRET,
     resave: false,
