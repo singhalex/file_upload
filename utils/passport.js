b132de587dadb294ffc98897b118b882bb642023
Alex Singh <115970252+singhalex@users.noreply.github.com>
2024-08-30 20:54:11 -0700

    Remove unneeded imports

--- utils/passport.js
+++ utils/passport.js
@@ -1,9 +1,5 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
-const bcrypt = require("bcryptjs");
-
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
 
 // Define the local strategy
 passport.use(
